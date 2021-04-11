1a3378c58f89c50857a2eed6d1666c7b47ddf036
Ashwin Kumar <ashwinkumar3399@gmail.com>
2021-04-12 00:17:23 +0530

    Added Migration; GratitudeJ Model

--- CopingWithCovid/physical/models.py
+++ CopingWithCovid/physical/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class GratitudeJournal(models.Model):
+	content = models.TextField()
+	date_posted = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
